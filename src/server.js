3504521fec877de63d34406688edd11a5a6ab624
TeqNickels <debrenamcewen@gmail.com>
2017-12-11 17:30:15 -0800

    Does initial passport setup before refactoring
    Facebook strategy currently allows us to access our GET /auth/facebook/callback after putting in our facebook login info

--- src/server.js
+++ src/server.js
@@ -1,10 +1,22 @@
-const bodyParser = require('body-parser');
 const express = require('express');
+const bodyParser = require('body-parser');
 const routes = require('./server/routes');
 const methodOverride = require('method-override');
 const path = require('path');
+const passport = require('passport');
+const session = require('express-session');
+require('dotenv').config();
+
+// const logger = require('morgan');
 
 const app = express();
+
+
+const sessionOptions = {
+  secret: 'm5a6r2l9b7o3r5o',
+  resave: 'true',
+  saveUnitialized: 'true',
+};
 
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
@@ -16,6 +28,15 @@ app.use(methodOverride('_method'));
 
 app.use('/', routes);
 
+app.use(session(sessionOptions));
+
+
+// Initialize passport
+app.use(passport.initialize());
+
+// Restore session
+app.use(passport.session());
+
 // app.use((request, response) => {
 //   response.render('common/not_found');
 // });
@@ -24,4 +45,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
 });
 
-module.exports = app;
+// module.exports = app;
