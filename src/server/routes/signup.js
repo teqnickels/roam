d80d2e07f02651dfd3a00a25d6ea21ed573830dc
TeqNickels <debrenamcewen@gmail.com>
2017-12-18 14:41:47 -0800

    add route for signup

--- src/server/routes/signup.js
+++ src/server/routes/signup.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
-
+const signUp = require('../../models/db/signup')
+const bodyParser = require('body-parser')
 
 router.get('/', (request, response) => {
   response.render('signup')
 })
 
+router.post('/', (request, response) => {
+    var email = request.body.email;
+    var password = request.body.password;
+    response.json('DONE')
+});
 module.exports = router;
