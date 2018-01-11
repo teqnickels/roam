647b68fd44fa00400ae7a9cb59a928cd635e11d9
TeqNickels <debrenamcewen@gmail.com>
2018-01-10 16:27:21 -0800

    add seed files

---
+++ src/models/db/schema/seedUsers.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+  users (first_name, last_name, email, password)
+VALUES
+  ('Jared', 'Grippe', 'jared@lg.org', '$2a$10$a.jueGqV.x/QCl/rIvRxq.B0G2wYLXPv7c3gzG0UdXq3Q/3sY9Me6'),
+  ('Tanner', 'Welsh','Tanner@lg.org', '$2a$10$a.jueGqV.x/QCl/rIvRxq.B0G2wYLXPv7c3gzG0UdXq3Q/3sY9Me6'),
+  ('NeEddra', 'James','Nee@lg.org', '$2a$10$a.jueGqV.x/QCl/rIvRxq.B0G2wYLXPv7c3gzG0UdXq3Q/3sY9Me6');
