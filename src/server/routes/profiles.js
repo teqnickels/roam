34ef0423e28a58c40eade5e860230fe06171e497
TeqNickels <debrenamcewen@gmail.com>
2018-02-02 15:08:04 -0800

    Change router.use('/posts', posts) to router.use('/profile', posts)

--- src/server/routes/profiles.js
+++ src/server/routes/profiles.js
@@ -4,10 +4,11 @@ const { posts } = require('../../models/db/posts')
 
 
 router.get('/:user_id/:blog_id', (request, response) => {
+  console.log('Hitting the route you think its hitting')
   const blogId = request.params.blog_id
   return posts.getPostById(blogId)
     .then((blog) => {
-      response.render('blogs', {blog: blog})
+      response.render('blogs', { blog })
     })
     .catch(console.error)
 });
